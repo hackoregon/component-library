f0a80ecaabc910b109cdc8c069e01165c762062a
drabelpdx <drabelpdx@gmail.com>
2017-02-05 15:05:07 -0800

    Add Leaflet component, story, and test files.

---
+++ components/Leaflet/Leaflet.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Leaflet from './Leaflet';
+
+describe('Leaflet', () => {
+  const data = 'data';
+
+  it('should render with Leaflet', () => {
+    const wrapper = shallow(<Leaflet data={data} />);
+    expect(wrapper).to.be.length(1);
+  });
+
+  it('should render with class mapSize', () => {
+    const wrapper = shallow(<Leaflet data={data} />);
+    expect(wrapper.props().className).to.contain('mapSize');
+  });
+
+  it('should render with zoom of 14', () => {
+    const wrapper = shallow(<Leaflet data={data} />);
+    expect(wrapper.props().zoom).to.equal(14);
+  });
+});
--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf, linkTo } from '@kadira/storybook'; // eslint-disable-line
 import buttonStory from './Button.story';
+import leafletStory from './Leaflet.story';
 import storycardStory from './StoryCard.story';
 import editableStory from './Editable.story';
 import pieStory from './Pie.story';
@@ -14,14 +15,15 @@ import '../components/global.styles.css';
 
 // stories can be added directly here
 storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')} />
-  ));
+.add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 // or imported as functions from files then composed in the order you invoke them
 editableStory();
 coreStory();
 buttonStory();
+leafletStory();
 storycardStory();
 pieStory();
 barChartStory();
