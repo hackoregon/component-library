bb3d10234d8df13d6be6c365f2cd3317f1ad352c
patrick lipscomb <patrick74lipscomb@gmail.com>
2017-02-05 20:48:24 -0800

    add tests for the leaflet map component, currently receiving an error however

--- components/LeafletMap/LeafletMap.test.js
+++ components/LeafletMap/LeafletMap.test.js
@@ -3,3 +3,13 @@ import { shallow } from 'enzyme';
 import LeafletMap from './LeafletMap';
 
 // Getting there, will update soon...
+// TESTING SAYS REFEENCE ERROR: WINDOW IS NOT DEFINED
+describe('LeafletMap', () => {
+  const wrapper = shallow(<LeafletMap />);
+  it('should expect to find a div element', () => {
+    expect(wrapper.find('div')).have.length(1);
+  })
+  it('should render the div with the id "map"', () => {
+    expect(wrapper.find('#map')).have.length(1);
+  });
+});
