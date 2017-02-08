3600b385497ef90a158eb887bb5df096ae2f0f5b
Justine Wang <justinelhwang@gmail.com>
2017-02-07 19:30:42 -0800

    made leaflet.css file in assets

--- components/LeafletMap/LeafletMap.js
+++ components/LeafletMap/LeafletMap.js
@@ -1,28 +1,32 @@
 import React, { PropTypes } from 'react';
 import { Map, TileLayer } from 'react-leaflet';
 import map from './LeafletMap.styles.css';
+// import . from '../../assets/leaflet.css';
 
-const data = { 
-	position: [45.51, -122.68], 
-	zoom: 13 
+const data = {
+  position: [45.51, -122.68],
+  zoom: 13
 };
 
-const LeafletMap = () => (
-	<Map id='map' center={data.position} zoom={data.zoom}>
-		<TileLayer
-			url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-			attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		/>
-	</Map>
-);
+const LeafletMap = () => {
+  require('../../assets/leaflet.css');
+  return (
+    <Map id='map' center={data.position} zoom={data.zoom}>
+      <TileLayer
+        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </Map>
+  );
+};
 
 // const LeafletMap = ({ data }) => (
-// 	<Map id='map' center={[45.51, -122.68]} zoom={13}>
-// 		<TileLayer
-// 			url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-// 			attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-// 		/>
-// 	</Map>
+//   <Map id='map' center={[45.51, -122.68]} zoom={13}>
+//     <TileLayer
+    // url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+//       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//     />
+//   </Map>
 // );
 
 LeafletMap.displayName = 'LeafletMap';
