a1f2b6e6851a0d66337e6c24917fd2886e2a5281
drabelpdx <drabelpdx@gmail.com>
2017-02-18 11:43:08 -0800

    Fetch returns and displays a table

--- src/ViewData/ViewData.js
+++ src/ViewData/ViewData.js
@@ -17,12 +17,19 @@ export default class ViewData extends React.Component {
 
     this.state = {
       target: {},
+      input: '',
     };
     this.goFetch = this.goFetch.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  updateInput(e) {
+    this.setState({ input: e.target.value });
   }
 
   goFetch = () => {
-    fetch('http://54.213.83.132/hackoregon/http/current_candidate_transactions_out/931/')
+    this.setState({ input: '' });
+    fetch(`http://54.213.83.132/hackoregon/http/current_candidate_transactions_out/${this.state.input}/`)
       .then(response =>  response.json())
       .then(stories => this.setState({ target: stories }))
       .catch(ex => console.log('failed', ex));
@@ -30,21 +37,57 @@ export default class ViewData extends React.Component {
 
   render() {
     const getInfo = this.state.target;
-    const info = [];
-
+    let header = [];
+    let rows = [];
+    //  if not empty, grabs a record to pull header tags
+    if (Object.keys(getInfo).length !== 0 && getInfo.constructor !== Object) {
+      const forHeader = getInfo[0];
+      header = Object.keys(forHeader).map((key) => {
+        return (
+          <th>{key}</th>
+        );
+      });
+    }
+    // wraps each item in a <td> tag
     for (let i = 0; i < getInfo.length; i += 1) {
       const pullInfo = Object.keys(getInfo[i]).map((key) => {
         return (
-          <li key={key}>{key}: {getInfo[i][key]}</li>
+          <td>{getInfo[i] !== null ? getInfo[i][key] : 'null'}</td>
         );
       });
-      info.push(pullInfo);
+      // builds rows list from each mapped record
+      rows.push(pullInfo);
     }
+    // warps each row with a <tr> tag
+    rows = rows.map((row, i) => {
+      return (
+        <tr key={i}>{row}</tr>
+      );
+    });
 
     return (
-      <div id="layout-content" className="layout-content-wrapper">
-        <Button onClick={this.goFetch}> get my data </Button>
-        <div className="panel-list">{info}</div>
+      <div>
+        <input
+          className="dontHaveOne"
+          type="text"
+          value={this.state.input}
+          onChange={this.updateInput}
+        />
+        <br />
+        <button
+          onClick={this.goFetch}
+          className="dontHaveOne"
+          type="submit"
+        >Fetch
+        </button>
+        <table>
+          <thead>
+            {header}
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
       </div>
     );
   }
