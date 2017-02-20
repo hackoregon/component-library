857340fec5b12fed50c73ef182b5f5175c96f538
Robert Tyree <tyreer@gmail.com>
2017-02-19 20:48:38 -0800

    Added isomorphic-fetch and es6-promise. Wrote a simple callback in componentDidMount() method of ViewData component. Accesses API endpoint and renders a value from that data object in an h3.

--- src/ViewData/ViewData.js
+++ src/ViewData/ViewData.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
 export default class ViewData extends Component {
-
   static displayName = 'View Data'
+
+  componentDidMount() {
+    require('es6-promise').polyfill();
+    require('isomorphic-fetch');
+
+    fetch('http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/5591/')
+    .then(function handleAJAX(response) {
+      if (response.status >= 400) {
+        throw new Error('Bad response from server');
+      }
+      return response.json();
+    })
+    .then(function callBack(data) {
+      document.getElementsByClassName('Data')[0].innerHTML = `<h3>${data[0].filer}</h3>`;
+    });
+  }
 
   render() {
     return (
       <div>
         <input />
         <button>Get Data</button>
+        <div className={'Data'} />
       </div>
     );
   }
