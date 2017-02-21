ff0e8b9a2a2d054bcc0d9b377cd38e9cf1cb867b
Justine Wang <justinelhwang@gmail.com>
2017-02-20 19:45:41 -0800

    Fixed the isometric fetch of the data with David's help, but ViewData still needs to be able to filter the JSON by year.

--- src/ViewData/ViewData.js
+++ src/ViewData/ViewData.js
@@ -2,52 +2,40 @@ import React, { Component } from 'react';
 import fetch from 'isomorphic-fetch';
 
 
-class ViewData extends Component {
+export default class ViewData extends Component {
   constructor(props) {
     super(props);
     // some local state to manage passing
     // data
     this.state = {
-      data: [],
+      data: {},
       // Before the component renders, it hasn't reached out to
       // the API yet, so we write a placeholder for the data first,
       // and later update it with a life cycle method.
     };
   }
 
-  componentWillMount() {
-    require('es6-promise').polyfill();
-    require('isomorphic-fetch');
 
-    // const campaigndata = fetch('http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/5591/')
-    //   .then((response) => {
-    //     return response.json();
-    //   }
-    // );
+  // require('es6-promise').polyfill();
+  // require('isomorphic-fetch');
 
+  // const campaigndata = fetch('http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/5591/')
+  //   .then((response) => {
+  //     return response.json();
+  //   }
+  // );
 
-    fetch('http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/5591/').then((response) => {
-      // this.setState({
-      //   id:
-      //   records:
-      // })
-      if (response.status >= 400) {
-        throw new Error('Bad response from server');
-      }
-      return response.json();
-    }).then((data) => {
-      this.state.data = data;
-      this.setState(this.state);
-    });
+  componentDidMount() {
+    console.log('fired?');
+    fetch('http://54.213.83.132/hackoregon/http/current_candidate_transactions_in/5591/')
+      .then(response => response.json())
+      .then(datas => this.setState({ data: datas }))
+      .then(console.log(this.state.data));
   }
 
   render() {
     return (
-      <div>
-        {this.state.data}
-      </div>
+      <div>{this.state.data}</div>
     );
   }
 }
-
-export default ViewData;
