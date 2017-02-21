bbe4c22a5431e4316c150f15b8e281171c873579
Robert Tyree <tyreer@gmail.com>
2017-02-20 17:03:59 -0800

    Added lifecycle methods with simple console.logs. Refactored fetch function into componentDidMount() and setState on the JSON response data there.

--- src/ViewData/ViewData.js
+++ src/ViewData/ViewData.js
@@ -12,13 +12,13 @@ export default class ViewData extends Component {
     this.onValueChange = this.onValueChange.bind(this);
   }
 
-  onValueChange = (e) => {
-    const value = e.target.value;
-    this.setState({ value });
-  };
+  // console.log all the LifeCycle Methods
+  componentWillMount() {
+    console.log('Component will mount');
+  }
 
-  // componentDidMount() {
-  clickHandler(input) {
+  componentDidMount = () => {
+    console.log('Component did mount');
     require('es6-promise').polyfill();
     require('isomorphic-fetch');
 
@@ -29,18 +29,48 @@ export default class ViewData extends Component {
       }
       return response.json();
     })
-    .then(function callBack(data) {
-      document.getElementsByClassName('Data')[0].innerHTML = `
-        <h3>This contributor: ${data[input].contributor_payee}</h3>
-        <h3>Contributed this amount: $${data[input].amount}</h3>`
-      // Attempt at matching zip code
-      // function filterCondition(dataInstance) {
-      //   dataInstance.zip === parseInt(input, 10)
-      // }
-      // const filteredArray = data.filter(filterCondition)
-      // console.log(filteredArray);
-    });
+    .then(function addAJAX(data) {
+      this.setState({ responseData: data })
+    }.bind(this));
   }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('Component will receive props', nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should component update', nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('Component will update', nextProps, nextState);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component did update', prevProps, prevState);
+  }
+
+  componentWillUnmount() {
+    console.log('Component will unmount');
+  }
+
+  onValueChange = (e) => {
+    const value = e.target.value;
+    this.setState({ value });
+  };
+
+  clickHandler(input, data) {
+    document.getElementsByClassName('Data')[0].innerHTML = `
+      <h3>This contributor: ${data[input].contributor_payee}</h3>
+      <h3>Contributed this amount: $${data[input].amount}</h3>`
+  }
+    // Attempt at matching zip code
+    // function filterCondition(dataInstance) {
+    //   dataInstance.zip === parseInt(input, 10)
+    // }
+    // const filteredArray = data.filter(filterCondition)
+    // console.log(filteredArray);
 
   render() {
     return (
@@ -51,7 +81,9 @@ export default class ViewData extends Component {
           onChange={this.onValueChange}
           placeholder="Enter a number"
         />
-        <button onClick={() => this.clickHandler(this.state.value)}>Fetch Data</button>
+        <button onClick={() => this.clickHandler(this.state.value, this.state.responseData)}>
+          Fetch Data
+        </button>
         <div className={'Data'} />
       </div>
     );
