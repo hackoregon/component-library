1487cf8eb47ec2389e42587e87e37d8c3edb73ee
Justine Wang <justinelhwang@gmail.com>
2017-02-22 14:56:19 -0800

    First pass at a filter function.

--- src/ViewData/ViewData.js
+++ src/ViewData/ViewData.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import fetch from 'isomorphic-fetch';
+// import fetch from 'isomorphic-fetch';
 
 
 export default class ViewData extends Component {
@@ -12,6 +12,9 @@ export default class ViewData extends Component {
       // Before the component renders, it hasn't reached out to
       // the API yet, so we write a placeholder for the data first,
       // and later update it with a life cycle method.
+      input: '',
+      // The ViewData class component has an input property to hold
+      // query terms by the user.
     };
   }
 
@@ -37,19 +40,30 @@ export default class ViewData extends Component {
   }
 
   // Filter jsondata from AJAX request to render only the objects with
-  // the given trans_id.
-  // filterByKey() {
+  // the given query.
+  // filterByYear() {
   //   jsondata.filter((record) => {
   //   return Object.keys(jsondata) === record;
   //   }
   // }
+
+  filterByYear() {
+    const filtered = (this.state.data.filter((record) => {
+      return record.tran_date.substring(0, 4).indexOf(this.state.input) !== -1;
+    }));
+    this.setState({ data: filtered });
+  }
 
   render() {
     // const info = this.state.data;
     console.log(this.state.data, ' this.state.data?');
 
     const records = this.state.data.map((obj, idx) => {
-      return <li key={idx}>{obj.tran_id}</li>
+      return (
+        <div>
+          <li key={idx}>{obj.tran_id}</li>
+        </div>
+      );
     });
     return (
       <div>
