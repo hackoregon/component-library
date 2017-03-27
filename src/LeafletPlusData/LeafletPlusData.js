8253475f9eb3036db6913c1b8e92d92ad2b96cc4
Robert Tyree <tyreer@gmail.com>
2017-03-26 17:51:40 -0700

    Added RechartsPie and extended neighborhood GeoJSON data with mock demographic data. Donut chart and neighborhood title change as expected on click.

--- src/LeafletPlusData/LeafletPlusData.js
+++ src/LeafletPlusData/LeafletPlusData.js
@@ -3,9 +3,45 @@ import { Map, TileLayer, GeoJSON } from 'react-leaflet';
 import classNames from 'classnames/bind';
 import styles from './LeafletMap.style.css';
 import neighborhoodGeoJSON from './neighborhoodGeoJson.json';
+import RechartsPie from '../RechartsPie/RechartsPie';
 
 const cx = classNames.bind(styles);
 const className = cx({ mapStyles: true });
+
+// data model
+const mockDemographicData = [
+  { name: 'White', value: 400 }, { name: 'Black', value: 300 },
+  { name: 'Asian', value: 300 }, { name: 'Hispanic', value: 200 },
+  { name: 'Native American', value: 200 },
+];
+
+const mockDemographicData2 = [
+  { name: 'White', value: 400 }, { name: 'Black', value: 400 },
+  { name: 'Asian', value: 400 }, { name: 'Hispanic', value: 400 },
+  { name: 'Native American', value: 400 },
+];
+
+const mockDemographicData3 = [
+  { name: 'White', value: 200 }, { name: 'Black', value: 500 },
+  { name: 'Asian', value: 400 }, { name: 'Hispanic', value: 100 },
+  { name: 'Native American', value: 100 },
+];
+
+// const geoJSONDemographics = neighborhoodGeoJSON.features.map(object => object.properties.DEMOGRAPHICDATA = mockDemographicData);
+
+const addDemographicData = (neighborhoodGeoJSON, mockDemographicData2, mockDemographicData3) => {
+  const geoData = neighborhoodGeoJSON;
+  geoData.features.forEach((t) => {
+    const geoRecord = t;
+    geoRecord.properties.OBJECTID % 2 === 0 ?
+    geoRecord.properties.DEMOGRAPHICDATA = mockDemographicData2 :
+    geoRecord.properties.DEMOGRAPHICDATA = mockDemographicData3;
+  });
+  return geoData;
+};
+
+const geoJSONDemographics = addDemographicData(neighborhoodGeoJSON, mockDemographicData2, mockDemographicData3);
+
 
 export default class LeafletPlusData extends Component {
 
@@ -17,22 +53,25 @@ export default class LeafletPlusData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      geoData: neighborhoodGeoJSON,
+      geoData: geoJSONDemographics,
       contData: '',
       reducedDataObject: '',
-      center: [45.52, -122.63],
-      zoom: 12,
+      center: this.props.position,
+      zoom: this.props.zoom,
       attribute: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       color: 'blue',
       neighborhoodFocus: '',
+      neighborhoodDemographicData: mockDemographicData,
     };
   }
 
   handleClick = (feature, layer) => {
     layer.on('click', (e) => {
+      console.log(feature.properties.DEMOGRAPHICDATA);
       this.setState({
         neighborhoodFocus: `${feature.properties.NAME}`,
+        neighborhoodDemographicData: feature.properties.DEMOGRAPHICDATA,
       });
     });
   }
@@ -54,6 +93,12 @@ export default class LeafletPlusData extends Component {
           <GeoJSON data={this.state.geoData} onEachFeature={this.handleClick} color={this.state.color} />
         </Map>
         <h1>Neighborhood: {this.state.neighborhoodFocus}</h1>
+        <RechartsPie
+          data={this.state.neighborhoodDemographicData}
+          chartProportions={this.props.chartProportions}
+          colors={this.props.colors}
+          styles={this.props.styles}
+        />
       </div>
     );
   }
--- stories/LeafletPlusData.story.js
+++ stories/LeafletPlusData.story.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { LeafletPlusData } from '../src';
+import { colors } from './shared';
 
 const displayName = 'LeafletPlusData';
 const title = 'Leaflet with data on and below map';
 const description = 'react-leaflet map of Portland with GeoJSON neighborhood data on map and desrived data beneath map on click';
 
-const position = [45.57, -122.67];
-const zoom = 9;
+const position = [45.52, -122.63];
+const zoom = 12;
 const maxzoom = 10;
+
+
+// This base can be adjusted to scale up or down the chart and legend
+const proportionBase = 200;
+
+// These multipliers can be adjusted to modify the individual
+const chartProportions = {
+  chartWidth: proportionBase * 3,
+  chartHeight: proportionBase * 1,
+  iconSize: proportionBase * 0.075,
+  pieInnerRadius: proportionBase * 0.2,
+  pieOuterRadius: proportionBase * 0.4,
+};
+
+// Styles here based on src/Pie/Pie.css
+const styles = {
+  fontFamily: 'filson-soft',
+  fontSize: proportionBase * 0.08,
+  fontWeight: 300,
+  color: '#706371',
+  fill: '#706371',
+};
 
 const demoCode = () => (
   <LeafletPlusData
@@ -19,6 +42,9 @@ const demoCode = () => (
     dragging={false}
     scrollWheelZoom={false}
     doubleClickZoom={false}
+    chartProportions={chartProportions}
+    colors={colors}
+    styles={styles}
   />
 );
 
