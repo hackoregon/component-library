8365696cecbae699d880b9dd57a8bb8761521cc9
Justine Wang <justinelhwang@gmail.com>
2017-04-09 14:45:42 -0700

    Copied my scatterplot files from the old-scatterplot branch. old-scatterplot was based off of my master branch and not the hackoregon/component-library master branch, so it was ahead.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export AreaChart from './AreaChart/AreaChart';
 export BarChart from './BarChart/BarChart';
 export Sankey from './Sankey/Sankey';
 export Button from './Button/Button';
@@ -7,16 +6,12 @@ export Chart from './Chart/Chart';
 export ChartData from './ChartData/ChartData';
 export Pie from './Pie/Pie';
 export Slider from './Slider/Slider';
-export Footer from './Footer/Footer';
-export ScrollToTop from './ScrollToTop/ScrollToTop';
 export Header from './Navigation/Header';
 export Nav from './Navigation/Nav';
 export NavRouterLink from './Navigation/NavRouterLink';
 export RechartsPie from './RechartsPie/RechartsPie';
 export LeafletMap from './LeafletMap/LeafletMap';
-export StamenMap from './StamenMap/StamenMap';
-export Dropdown from './Dropdown/Dropdown';
-export Icon from './Icon/Icon';
+export Scatterplot from './Scatterplot/Scatterplot';
 
 // export utils as well for broader use
 export isClient from './utils/isClient';
