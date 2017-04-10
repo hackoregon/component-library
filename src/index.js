b259701aa4f87c25d4123838fc4e25d5976e45ce
Justine Wang <justinelhwang@gmail.com>
2017-04-10 11:20:30 -0700

    Added code for scatterplot to src/index.js and stories/index.js after pulling most recent changes from hackoregon/master.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export AreaChart from './AreaChart/AreaChart';
 export BarChart from './BarChart/BarChart';
 export Sankey from './Sankey/Sankey';
 export Button from './Button/Button';
@@ -6,11 +7,16 @@ export Chart from './Chart/Chart';
 export ChartData from './ChartData/ChartData';
 export Pie from './Pie/Pie';
 export Slider from './Slider/Slider';
+export Footer from './Footer/Footer';
+export ScrollToTop from './ScrollToTop/ScrollToTop';
 export Header from './Navigation/Header';
 export Nav from './Navigation/Nav';
 export NavRouterLink from './Navigation/NavRouterLink';
 export RechartsPie from './RechartsPie/RechartsPie';
 export LeafletMap from './LeafletMap/LeafletMap';
+export StamenMap from './StamenMap/StamenMap';
+export Dropdown from './Dropdown/Dropdown';
+export Icon from './Icon/Icon';
 export Scatterplot from './Scatterplot/Scatterplot';
 
 // export utils as well for broader use
