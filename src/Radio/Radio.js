5debcfe8eaf70f59a8bc44af45967f4839eb7c58
Xaiana <melisskann@gmail.com>
2017-04-11 18:36:38 -0700

    updated radio

--- src/Radio/Radio.js
+++ src/Radio/Radio.js
@@ -6,49 +6,48 @@ class Radio extends React.Component {
 
   constructor(props) {
     super(props);
-    this.checkIt = this.checkIt.bind(this);
-    this.unCheckIt = this.unCheckIt.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.state = {
-      checked:false
+      checked: false
     };
   }
 
-  checkIt() {
-    this.setState({
-      checked:true
-    });
-  }
-
-  unCheckIt() {
-    this.setState({
-      checked:false
-    });
-  }
-
   handleChange(evt) {
-    if(this.state.checked !== evt.target.checked) {
-      this.setState({
-        checked:evt.target.checked
-      });
-    }
+    this.setState({
+      checked: evt.target.value
+    });
+  }
+
+  handleFormSubmit(evt) {
+    console.log('You have selected:', this.state.checked);
+    evt.preventDefault();
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <div class="radio">
-          <label><input type="radio" name="option" checked={this.state.checked} onChange={this.handleChange}/>Option 1</label>
+          <label>
+            <input type="radio" value="option1" checked={this.state.checked==="option1"} onChange={this.handleChange}/>Option 1
+          </label>
         </div>
         <div class="radio">
-          <label><input type="radio" name="option" checked={this.state.checked} onChange={this.handleChange}/>Option 2</label>
+          <label>
+            <input type="radio" value="option2" checked={this.state.checked==="option2"} onChange={this.handleChange}/>Option 2
+          </label>
         </div>
         <div class="radio">
-          <label><input type="radio" name="option" checked={this.state.checked} onChange={this.handleChange}/>Option 3</label>
+          <label>
+            <input type="radio" value="option3" checked={this.state.checked==="option3"} onChange={this.handleChange}/>Option 3
+          </label>
         </div>
         <div class="radio">
-          <label><input type="radio" name="option" checked={this.state.checked} onChange={this.handleChange}/>Option 4</label>
+          <label>
+            <input type="radio" value="option4" checked={this.state.checked==="option4"} onChange={this.handleChange}/>Option 4
+          </label>
         </div>
+        <button class="btn btn-default" type="submit">Submit</button>
       </form>
     )
   }
@@ -61,5 +60,4 @@ Radio.propTypes = {
 Radio.defaultProps = {
 
 }
-
 export default Radio;
--- src/stories/Radio.story.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import { Radio } from '../src';
