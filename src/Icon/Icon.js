fca4badb6191343ca5869ef63487a067685b31e1
David Daniel <davidedaniel@gmail.com>
2017-05-01 08:03:26 -0700

    specificity

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Icon = ({ className, handleClick }) => (
-  <span><i onClick={handleClick} className={className} /></span>
+  <span><i onClick={handleClick} className={className} aria-hidden="true" /></span>
 );
 
 Icon.propTypes = {
